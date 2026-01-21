4a44ec3bb69a9683aa6423cae2f98931345f3ca6
かずき <ka10fukuda28@icloud.com>
2026-01-22 07:54:47 +0900

    ホーム画面のルーティングを/で遷移できるよう修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # ホーム画面の仮設定
-  get "home/top" => "home#top"
+  root "home#top"
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
