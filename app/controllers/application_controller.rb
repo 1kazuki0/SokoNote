d681bf88df4c121bd6d96f57908d5f47dcb94f3c
かずき <ka10fukuda28@icloud.com>
2026-01-22 13:13:25 +0900

    新規登録・ログイン後の画面遷移

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  def after_sign_in_path_for(resources)
+    items_path
+  end
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
