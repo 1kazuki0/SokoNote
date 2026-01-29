1c9b86304fa449c479917ecb47628d1e77d48e94
かずき <ka10fukuda28@icloud.com>
2026-01-30 07:55:51 +0900

    rubocop修正

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # ログイン後のホーム画面（商品一覧画面）の設定
   resources :items
   # 設定画面
-  get 'setting', to: "setting#index"
+  get "setting", to: "setting#index"
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
