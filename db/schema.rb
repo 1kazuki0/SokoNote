d16318a4f3fa3b352d4daf18e9e1cbc099a10096
かずき <ka10fukuda28@icloud.com>
2026-02-01 13:30:47 +0900

    店舗テーブルのマイグレーション・モデルの作成・編集

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_01_30_120911) do
+ActiveRecord::Schema[7.2].define(version: 2026_02_01_033428) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,14 @@ ActiveRecord::Schema[7.2].define(version: 2026_01_30_120911) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
+  create_table "stores", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_stores_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -36,4 +44,5 @@ ActiveRecord::Schema[7.2].define(version: 2026_01_30_120911) do
   end
 
   add_foreign_key "items", "users"
+  add_foreign_key "stores", "users"
 end
