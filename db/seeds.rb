0347659307c365661f805002f756b82bfe1d53ce
かずき <ka10fukuda28@icloud.com>
2026-02-04 16:06:22 +0900

    商品一覧画面作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,21 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# 10.times do
+#   User.create(name: Faker::Name.first_name,
+#               email: Faker::Internet.unique.email,
+#               password: "password")
+# end
+
+# user_id:22 category_id:4を固定し代入
+user = User.find(22)
+category = Category.find(4)
+# 10回繰り返し
+10.times do
+  Item.create(user: user,
+              category: category,
+              name: Faker::Food.unique.vegetables) #uniqueで今まで出したデータと被らないようにする。
+end
+
+# docker compose exec web rails db:seedで実行すると10件分のItemが生成される
