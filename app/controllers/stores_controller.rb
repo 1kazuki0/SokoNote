6123b1d889ca10323c86899f033da1cb528308f5
かずき <ka10fukuda28@icloud.com>
2026-04-12 22:35:32 +0900

    店舗マスタの新規登録実装

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -4,9 +4,17 @@ class StoresController < ApplicationController
   end
 
   def new
+    @store = current_user.stores.new()
   end
 
   def create
+    @store = current_user.stores.new(store_params)
+    if @store.save
+      redirect_to stores_path, success: "店舗を登録しました"
+    else
+      flash.now[:error] = "店舗登録に失敗しました"
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
@@ -16,5 +24,11 @@ class StoresController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def store_params
+    params.require(:store).permit(:name)
   end
 end
