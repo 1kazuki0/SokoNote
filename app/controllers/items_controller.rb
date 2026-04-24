da29371574d71d7f301d4dc956c8ed2eccba74d2
かずき <ka10fukuda28@icloud.com>
2026-04-24 10:22:13 +0900

    商品一覧画面に文字検索機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,5 +2,6 @@ class ItemsController < ApplicationController
   def index
     @q = current_user.items.ransack(params[:q])
     @items = @q.result(distinct: true).includes(:category, :purchases, purchases: [ :store, :content_unit, :pack_unit ])
+    @has_any_items = current_user.items.exists?
   end
 end
