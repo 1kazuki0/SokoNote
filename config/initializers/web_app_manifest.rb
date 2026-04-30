a97481f929e36bb8b1ee22416dacfe35f7a7a2d7
かずき <ka10fukuda28@icloud.com>
2026-04-30 13:51:26 +0900

    faat:ファビコン画像設定

---
+++ config/initializers/web_app_manifest.rb
@@ -0,0 +1,16 @@
+# This makes files with .webmanifest extension first class files in the asset
+# pipeline. This is to preserve this extension, as is it referenced in a call
+# to asset_path in the _favicon.html.erb partial.
+
+# .webmanifestというファイルをRailsのアセットパイプラインで使えるようにする設定
+Rails.application.config.assets.configure do |env|
+  mime_type = 'application/manifest+json'
+  extensions = ['.webmanifest']
+
+  if Sprockets::VERSION.to_i >= 4
+    extensions << '.webmanifest.erb'
+    env.register_preprocessor(mime_type, Sprockets::ERBProcessor)
+  end
+
+  env.register_mime_type(mime_type, extensions: extensions)
+end
