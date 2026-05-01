a7901198feb0a6bc4c4559de9ea1d04579b2ad25
かずき <ka10fukuda28@icloud.com>
2026-05-01 19:14:02 +0900

    perf: 商品削除時の条件分岐実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,7 +9,12 @@ class ItemsController < ApplicationController
   # 商品名削除用アクション
   def destroy
     item = current_user.items.find(params[:id])
-    item.destroy
-    redirect_to items_path, success: "「#{item.name}」を削除しました"
+    if item.purchases.exists?
+      redirect_to item_purchases_path(item), error: "購入履歴がある商品は削除できません"
+    elsif item.destroy
+      redirect_to items_path, success: "「#{item.name}」を削除しました"
+    else
+      redirect_to item_purchases_path(item), error: "商品の削除に失敗しました"
+    end
   end
 end
