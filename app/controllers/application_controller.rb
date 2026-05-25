95c88a968d4c78adf47a4a871e266ca900129caa
かずき <ka10fukuda28@icloud.com>
2026-05-26 08:25:08 +0900

    fix: permission_policyの再設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   # アクション実行前にログインしているか確認
   before_action :authenticate_user!
+  # セキュリティヘッダーのpermissions_policyの事前処理
+  before_action :set_permissions_policy_header
 
   def after_sign_in_path_for(resources)
     items_path
@@ -9,4 +11,10 @@ class ApplicationController < ActionController::Base
   allow_browser versions: :modern
 
   add_flash_types :success, :notice, :error
+
+  # configに設定しているpermissions_policyがfeature-policyに変換されるため
+  # controllerにて直接設定（セキュリティヘッダー）
+  def set_permissions_policy_header
+    response.headers["Permissions-Policy"] ="camera=(), gyroscope=(), microphone=(), usb=(), payment=(), geolocation=(), fullscreen=(self)"
+  end
 end
