dfd168f60539f4ebec546494c5c09a6285e5ed3e
かずき <ka10fukuda28@icloud.com>
2026-07-19 13:47:03 +0900

    chore: Railsバージョン8のアップデート

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_04_14_005128) do
+ActiveRecord::Schema[8.0].define(version: 2026_07_19_023757) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension "pg_catalog.plpgsql"
 
   create_table "categories", force: :cascade do |t|
     t.string "name", null: false
